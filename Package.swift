9f65160f42f9d407e324a1945842470f729a2de6
coolone <coolone.official@gmail.com>
2021-08-04 18:11:43 +0400

    Change publish dep

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", .upToNextMinor(from: "0.8.0")),
+        .package(name: "Publish", url: "https://github.com/CoolONEOfficial/publish.git", .branch("master")),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin.git", from: "0.1.0"),
         .package(name: "DarkImagePublishPlugin", url: "https://github.com/insidegui/DarkImagePublishPlugin.git", from: "0.1.0"),
         .package(name: "TinySliderPublishPlugin", url: "https://github.com/CoolONEOfficial/TinySliderPlugin.git", from: "0.1.3"),
